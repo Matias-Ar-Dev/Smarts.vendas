2bd2b80314f036af670063ba7c33e9cf0c1a4b29
Matias Armando <matiasdev650@gmail.com>
2025-06-26 08:10:04 +0100

    deploy

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL:'https://smarts-vendas-node.onrender.com'
+  // 'http://localhost:3000'
 
 })
